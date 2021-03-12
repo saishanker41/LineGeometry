b9c376e9718e75f4eac3a33f11dd03309f3948e7
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-13 01:33:21 +0530

    [sai] Calculated distance btwn two points

--- LineComparisonComputaion.java
+++ LineComparisonComputaion.java
@@ -3,5 +3,12 @@ public class LineComparisonComputaion {
 
       System.out.println("Welcome to Line Comparison Program on Master Branch");
 
+      int x1,x2,y1,y2;
+      double distance;
+      x1=1; y1=1; x2=4; y2=4;
+      distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+            System.out.println("distance between" + "("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+distance);
+
+
    }
 }
